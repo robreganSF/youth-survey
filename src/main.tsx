8f7fee31f4dbb06e6be9edd8951c39de81ddc742
Rob <rbregan@yahoo.com>
2025-05-15 21:38:39 -0700

    Show SurveyWrapper instead of default App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Or whatever global CSS you're using
+import SurveyWrapper from './SurveyWrapper.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SurveyWrapper />
+  </React.StrictMode>
+);
