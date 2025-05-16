5d694cbd418a57f2df26428d7f9da450f33413a8
Rob <rbregan@yahoo.com>
2025-05-15 21:42:07 -0700

    Switch entry to SurveyWrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Or whatever global CSS you're using
-import SurveyWrapper from './SurveyWrapper.tsx';
+import './index.css';
+import SurveyWrapper from '../SurveyWrapper';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SurveyWrapper />
   </React.StrictMode>
 );
+
